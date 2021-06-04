4c5aecf5c8094c097fc97cc4ac5406b2c225aa5d
Ievgen-Rybalko <jackrjack1@gmail.com>
2021-06-04 12:22:30 +0300

    upd: css added; js console.logs deleted

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ import API from './js/fetchCountries';
 import { error } from '@pnotify/core/dist/PNotify.js';
 import '@pnotify/core/dist/BrightTheme.css';
 import '@pnotify/core/dist/PNotify.css';
-// import '@pnotify/mobile/dist/PNotifyMobile.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
 
 
 const refs = getRefs();
 
 refs.searchQuery.addEventListener('input', debounce(onInput, 500));
-function onInput(e) {
-    const searchQueryValue = e.target.value;  //;form.elements.query.value
-    console.log(searchQueryValue);
+function onInput(evt) {
+    const searchQueryValue = evt.target.value;  //;form.elements.query.value
+    //console.log(searchQueryValue);
     refs.countryContainer.innerHTML = ''; //clear the markup
     API.fetchCountries(searchQueryValue)
         .then(data => {
@@ -24,21 +24,21 @@ function onInput(e) {
             if (data.status === 404) {
                 pNotyfyMassage('Nothing was found for your query!')
             }
-            else if (data.length > 10) {
-                pNotyfyMassage('Too many matches found. Please enter more specific query!');
-            }
             else if (data.length === 1) {
-              console.log('data1:', data);
+              //console.log('data1:', data);
                 const countryMarkup = createCardMarkup(data);
                 refs.countryContainer.insertAdjacentHTML('beforeend', countryMarkup);
                 refs.searchQuery.value = '';
             }
             else if (1 < data.length < 10) {
-              console.log('data10:', data);
+              //console.log('data10:', data);
                 const contryDropListMarkup = createCountryDropList(data);
                 refs.countryContainer.insertAdjacentHTML('beforeend', contryDropListMarkup);
-
             }
+            else if (data.length > 10) {
+                pNotyfyMassage('Too many matches found. Please enter more specific query!');
+            }  
+ 
         }).catch(onError)
         .finally(() => refs.searchQuery = '');
 };
@@ -55,7 +55,6 @@ function createCardMarkup(data) {
 
 function onError(error) {
     pNotyfyMassage(error);
-    //console.log(error);
 };
 
 function pNotyfyMassage(message) {
